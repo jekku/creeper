b8b0e9d9221f722a310bf266bab60abc91f53683
Jekri <jekri.orlina@any.tv>
2015-08-21 10:46:42 +0800

    Created initial start function.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+var movements = require('./lib/movements.js'),
+
+    start = function () {
+        while(true) {
+            movements.move_vertical(5);
+            movements.move_horizontal(5);
+        }
+    };
+
+start();
+
