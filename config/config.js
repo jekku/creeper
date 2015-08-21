6ffd1b9992970a0b911a52ba70dc34b001c7353f
Jekri <jekri.orlina@any.tv>
2015-08-21 14:34:34 +0800

    Added key shortcuts.

---
+++ config/config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    keyboard_shortcuts : {
+        change_window : ['alt', 'tab'],
+        new_tab : ['control','shift', 'T'],
+        new : ['control', 'N'],
+        save : ['control', 'S']
+    }
+};
