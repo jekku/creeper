48fe9c0141f047e60d0080322bbb08e1603f3fd9
Jekri <jekri.orlina@any.tv>
2015-08-21 14:49:53 +0800

    Added initial keyboard support. Deciding to make this a library.

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 'use strict';
-
-var do_some = require('./activities/activity_set.js'),
-
-    start = function () {
-        do_some.erratic_mouse(100, 5000, false);
-    };
-
-start();
--- movements/keyboard.js
+++ movements/keyboard.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var config = require('../config/config.js'),
+    robot = require('robotjs');
+
 exports.press_simultaneous = function (array) {
     array.forEach(function(keypress) {
         robot.keyToggle(keypress,true);
@@ -8,4 +11,12 @@ exports.press_simultaneous = function (array) {
     array.forEach(function(keypress) {
         robot.keyToggle(keypress,false);
     });
+};
+
+exports.do_shortcut = function (shortcut_code) {
+    if(!config.keyboard_shortcuts[shortcut_code]) {
+        return console.err('Shortcut code ' + shortcut_code 
+                            + ' does not exist.');
+    }
+    exports.press_simultaneous(config.keyboard_shortcuts[shortcut_code]);
 };
