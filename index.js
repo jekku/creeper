3a753a5cd72512bc31cb81b720b0a2062a143d30
Jekri <jekri.orlina@any.tv>
2015-08-21 14:59:38 +0800

    Present issue to robotjs owner.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 'use strict';
+
+require('./movements/keyboard').type_in(5, "Hello world!\n");
--- movements/keyboard.js
+++ movements/keyboard.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var config = require('../config/config.js'),
-    robot = require('robotjs');
+    robot = require('robotjs'),
+    sleeper = require('sleep');
 
 exports.press_simultaneous = function (array) {
     array.forEach(function(keypress) {
@@ -19,4 +20,9 @@ exports.do_shortcut = function (shortcut_code) {
             shortcut_code + ' does not exist.');
     }
     exports.press_simultaneous(config.keyboard_shortcuts[shortcut_code]);
+};
+
+exports.type_in = function (prep_time, string) {
+    sleeper.sleep(prep_time);
+    robot.typeString(string);
 };
