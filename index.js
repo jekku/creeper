42db3bbc753c19950670b02e0c547e9360b19eb9
Jekri <jekri.orlina@any.tv>
2015-08-21 10:54:21 +0800

    Select from three random actions.

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ var movements = require('./lib/movements.js'),
     _ = require('lodash'),
 
     start = function () {
+        var random_move_index;
         while(true) {
-            movements.move_vertical(_.random(-100,100));
-            movements.move_horizontal(_.random(-100,100));
+            random_move_index = _.random(0,2);
+
+            switch (random_move_index) {
+                case 0 :
+                    movements.move_vertical(_.random(-100,100));
+                break;
+
+                case 1 :
+                    movements.move_horizontal(_.random(-100,100));
+                break;
+
+                case 2 :
+                    movements.move_diagonal(_.random(-100,100),_.random(-100,100));
+                break;
+            }
         }
     };
 
