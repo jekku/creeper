589b7976e873174464669e086e855f5a17c7ac3e
Jekri <jekri.orlina@any.tv>
2015-08-21 14:34:43 +0800

    Renamed folder.

---
+++ movements/mouse.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var robot = require('robotjs'), 
+    mouse_pos;
+
+exports.update_mouse_location = function () {
+    mouse_pos = robot.getMousePos();
+    console.log("Position now at : " + mouse_pos);
+};
+
+exports.move_vertical = function (offset_y) {
+    exports.update_mouse_location();
+    robot.moveMouseSmooth(mouse_pos.x, mouse_pos.y + offset_y);
+};
+
+exports.move_horizontal = function (offset_x) {
+    exports.update_mouse_location();
+    robot.moveMouseSmooth(mouse_pos.x + offset_x, mouse_pos.y);
+};
+
+exports.move_diagonal = function (offset_x, offset_y) {
+    exports.update_mouse_location();
+    robot.moveMouseSmooth(mouse_pos.x + offset_x, mouse_pos.y + offset_y);
+};
+
+exports.do_click = function () {
+    robot.mouseClick();
+};
+
+exports.press_simultaneous = function (array) {
+    array.forEach(function(keypress) {
+        robot.keyToggle(keypress,true);
+    });
+
+    array.forEach(function(keypress) {
+        robot.keyToggle(keypress,false);
+    });
+};
